0734ec4f40e840879bed2f20cfd3a1bd455adbc3
Miriam Diaz <miriam101d@gmail.com>
2025-04-22 13:32:14 +0200

    requisitos update, inicio sopa

---
+++ sopa.js
@@ -0,0 +1,22 @@
+
+let points=0;
+let wordList=[
+    "Abanico",
+    "Barco",
+    "Sueño",
+    "Hurraca",
+    "Dificil",
+    "Jinete",
+    "Sal",
+    "Plomo",
+    "Cafe",
+    "Junta"
+];
+
+function createBoard(wordList){
+    let board;
+    for (let value of wordList){
+        
+    }
+    return board,
+}
